0d2edcbe577c6a50649268ee63930c43e01ca44e
Ben Newman <bn@cs.stanford.edu>
2010-04-26 20:10:49 -0700

    Delay Selection.fromString as well as sel.scrollTo until layout is more likely to be finished.

--- banter.js
+++ banter.js
@@ -6,20 +6,24 @@ var Selection = require("tricks/dom/selection").Selection,
         color: "#000",
         background: "lightgreen",
         padding: "2px 0 3px 0"
-    };
+    },
+    userOverride;
     
 if (splat.length > 1)
     whenReady(function() {
-        var sel = Selection.fromString(decodeURIComponent(splat.pop()));
-        if (sel) {
-            sel.wrap(styles);
-            setTimeout(function() {
+        setTimeout(function() {
+            if (userOverride)
+                return;
+            var sel = Selection.fromString(decodeURIComponent(splat.pop()));
+            if (sel) {
+                sel.wrap(styles);
                 sel.scrollTo(10);
-            }, 10);
-        }
+            }
+        }, 10);
     });
 
 addEvent(document, "mouseup", function() {
+    userOverride = true;
     var sel = Selection.getCurrent();
     if (sel) {
         var splat = location.href.split("#");
